1170941dec11505d15c3efa667bc9df02b8b50f2
watchmedash <mojazer2017@gmail.com>
2023-09-24 12:11:29 +0400

    up

--- script.js
+++ script.js
@@ -1,280 +1,39 @@
+document.addEventListener('DOMContentLoaded', () => {
+  let movieData; // Declare movieData here
 
+  // Function to load movie data from "home.html"
+  function loadMovieData() {
+    fetch('home.html')
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.text();
+      })
+      .then((html) => {
+        const tempElement = document.createElement('div');
+        tempElement.innerHTML = html;
 
-document.addEventListener('DOMContentLoaded', (event) => {
-  const itemList = [
-    "The Retirement Plan",
-    "American Pie Presents - Girls Rules",
-    "Dirty Grandpa",
-    "Midway",
-    "Spy Kids - Armageddon",
-    "No One Will Save You",
-    "Joe Dirt",
-    "Creation of the Gods I - Kingdom of Storms",
-    "Joe Dirt 2 - Beautiful Loser",
-    "Trevor Noah - I Wish You Would",
-    "Expend4bles",
-    "The Haunting at Saint Joseph s",
-    "Moneyball",
-    "Ligaw na Bulaklak",
-    "Jeanne du Barry",
-    "Retribution",
-    "Island Escape",
-    "Love at First Sight",
-    "Grown Ups",
-    "Grown Ups 2",
-    "The Hangover",
-    "The Hangover Part II",
-    "The Hangover Part III",
-    "The Island",
-    "Mother, May I",
-    "Ehrengard - The Art of Seduction",
-    "Real Steel",
-    "Once Upon a Crime",
-    "Elevator Game",
-    "A Haunting in Venice",
-    "Big Stan",
-    "Hugot",
-    "Tayuan",
-    "Paupahan",
-    "Lawa",
-    "Lagaslas",
-    "Hosto",
-    "Hilom",
-    "AFAM",
-    "Litsoneras",
-    "Manyak",
-    "Sandwich",
-    "Bisyo! Adik Ka Ba",
-    "The Adults",
-    "Patikim-tikim",
-    "Sakra",
-    "Ahsoka",
-    "Birth-Rebirth",
-    "The Seven Deadly Sins Grudge of Edinburgh",
-    "The Seven Deadly Sins Grudge of Edinburgh Part 2",
-    "Manhole",
-    "7-11 PM",
-    "Captive",
-    "Careful What You Wish For",
-    "Disturbia",
-    "Jawan",
-    "Talk to Me",
-    "Three Thousand Years of Longing",
-    "Sitting in Bars with Cake",
-    "I Am Legend",
-    "The Nun II",
-    "Sentinelle",
-    "Jailer",
-    "Call Me Alma",
-    "Punit na Langit",
-    "Strays",
-    "Here Comes the Boom",
-    "King of Killers",
-    "Hypnotic",
-    "Blended",
-    "Gran Turismo",
-    "Oppenheimer",
-    "Blue Beetle",
-    "The Equalizer 3",
-    "Mission Impossible - Dead Reckoning Part One",
-    "Barbie",
-    "Home Service",
-    "Bugaw",
-    "Kamadora",
-    "Kahalili",
-    "Die Hart",
-    "Teenage Mutant Ninja Turtles Mutant Mayhem",
-    "Ang mga kaibigan ni Mama Susan",
-    "Nightbird",
-    "One Piece",
-    "Operation Napoleon",
-    "Sex Games",
-    "The Forbidden Kingdom",
-    "The Last Voyage of the Demeter",
-    "Indiana Jones and the Dial of Destiny",
-    "Fall Guy",
-    "The Boogeyman",
-    "Kabayo",
-    "The Wall",
-    "Tell Me a Creepy Story",
-    "The Dive",
-    "The Monkey King",
-    "Salamat Daks",
-    "Haunting of the Queen Mary",
-    "Summer",
-    "Ride On",
-    "Vacation Friends 2",
-    "Meg 2 The Trench",
-    "Wrong Turn",
-    "Wrong Turn 2 Dead End",
-    "Wrong Turn 3 Left for Dead",
-    "Wrong Turn 4 Bloody Beginnings",
-    "Wrong Turn 5 Bloodlines",
-    "Wrong Turn 6 Last Resort",
-    "Wrong Turn 2021",
-    "Past Lives",
-    "Hacksaw Ridge",
-    "Sugapa",
-    "Doraemon the Movie - Nobitas Sky Utopia",
-    "The First Slam Dunk",
-    "Suki",
-    "One Piece Film Red",
-    "Puppy Love",
-    "The Communion Girl",
-    "The Fast and the Furious - Tokyo Drift",
-    "Kevin Hart Reality Check",
-    "Jimmy O Yang Guess How Much",
-    "No Hard Feelings",
-    "Escape Room Tournament of Champions",
-    "Black Widow",
-    "Birds of Prey",
-    "Justice League Dark Apokolips War",
-    "Jujutsu Kaisen 0",
-    "The Batman",
-    "The Forever Purge",
-    "Hitman's Wife's Bodyguard",
-    "Frozen II",
-    "Angel Has Fallen",
-    "The Invisible Man",
-    "Anna",
-"A Quiet Place Part II",
-"Antman and the Wasp Quantumania",
-"F9 The Fast Saga",
-"65",
-"Jungle Cruise",
-"Ready or Not",
-"Mulan",
-"Jumanji The Next Level",
-"Don't Breathe 2",
-"Free Guy",
-"Bad Boys for Life",
-    "Elemental",
-    "Heart of Stone",
-"Zom 100 - bucket list of the dead",
-"SpongeBob SquarePants Presents The Tidal Zone",
-    "1917",
-    "I Am Rage",
-    "The Collective",
-    "The Three Musketeers D Artagnan",
-    "Hidden Strike",
-    "Insidious - The Red Door",
-"365 Days This Day",
-"Army of the Dead",
-"Army of Thieves",
-"As Certain as Death",
-"Asteroid City",
-"Avatar The Way of Water",
-"Batman - Soul of the Dragon",
-"Batman - The Doom That Came to Gotham",
-"Batman and Superman - Battle of the Super Sons",
-"Beast",
-"Bird Box Barcelona",
-"Black Adam",
-"Black Clover Sword of the Wizard King",
-"Bullet Train",
-"Call Her King",
-"Chaos Walking",
-"Cocaine Bear",
-"DC League of Super - Pets",
-"Death on the Nile",
-"Demon Slayer the Movie - Mugen Train",
-"Doctor Strange in the Multiverse of Madness",
-"Dog",
-"Dune",
-"Dungeons & Dragons - Honor Among Thieves",
-"El Camino - A Breaking Bad Movie",
-"Enola Holmes 2",
-"Eternals",
-"Extraction 2",
-"Fantastic Beasts - The Secrets of Dumbledore",
-"Fast & Furious Presents - Hobbs & Shaw",
-"Fast X",
-"Firestarter",
-"Fullmetal Alchemist - The Final Alchemy",
-"Ghosted",
-"Glass Onion - A Knives Out Mystery",
-"God Is a Bullet",
-"Godzilla vs Kong",
-"Goodbye, Don Glees!",
-"Guardians of the Galaxy Volume 3",
-"Halloween Kills",
-"Home Sweet Home Alone",
-"Hotel Transylvania - Transformania",
-"Injustice",
-"John Wick Chapter 4",
-"Joker",
-"Jurassic World Dominion",
-"Justice League x RWBY - Super Heroes & Huntsmen, Part One",
-"Justice League-Warworld",
-"Justice Society - World War II",
-"Knights of the Zodiac",
-"Lightyear",
-"Luck",
-"Me Time",
-"Mighty Morphin Power Rangers Once & Always",
-"Minions - The Rise of Gru",
-"Morbius",
-"Mortal Kombat",
-"Nimona",
-"No Time to Die",
-"Nobody",
-"Nope",
-"Operation Fortune - Ruse de Guerre",
-"Orphan - First Kill",
-"Pinocchio",
-"Pokémon Detective Pikachu",
-"Prey",
-"Quicksand",
-"Raya and the Last Dragon",
-"Red Notice",
-"Renfield",
-"Resident Evil - Death Island",
-"Resident Evil - Welcome to Raccoon City",
-"Ruby Gillman  Teenage Kraken",
-"Samaritan",
-"scream 6",
-"Shang-Chi and the Legend of the Ten Rings",
-"Shazam! Fury of the Gods",
-"Sisu",
-"Sonic the Hedgehog 2",
-"Spider-Man - No Way Home",
-"Spider-Man Across the Spider-Verse",
-"Terminator - Dark Fate",
-"Texas Chainsaw Massacre",
-"The Addams Family 2",
-"The Black Demon",
-"The Blackening",
-"The Conjuring - The Devil Made Me Do It",
-"The Croods - A New Age",
-"The Flash",
-"The Gray Man",
-"The Lion King",
-"The Little Mermaid",
-"The Lost City",
-"The Machine",
-"The Matrix Resurrections",
-"The Mitchells vs The Machines",
-"The Northman",
-"The Out-Laws",
-"The Secret Life of Pets 2",
-"The Super Mario Bros Movie",
-"Thor - Love and Thunder",
-"Tom Clancys Without Remorse",
-"Top Gun - Maverick",
-"Toy Story 4",
-"Transformers - Rise of the Beasts",
-"Turning Red",
-"Uncharted",
-"Venom- Let There Be Carnage",
-"Violent Night",
-"Weathering With You",
-"Werewolf by Night",
-"Wonder Woman - Bloodlines",
-"Wrath of Man",
-"Zack Snyders Justice League",
+        const movieFigures = tempElement.querySelectorAll('figure[data-genre][data-release-year]');
+        movieData = Array.from(movieFigures).map((figure) => {
+          const title = figure.querySelector("figcaption").textContent;
+          const imageSrc = figure.querySelector("img.defer-image").getAttribute("data-src");
+          return { title, imageSrc };
+        });
 
-  ];
-  function performSearch() {
+        // Debugging: Log the loaded movie data
+        console.log("Movie Data Loaded:", movieData);
+
+        // Perform initial search
+        performSearch(movieData);
+      })
+      .catch((error) => {
+        console.error('Error loading movie data:', error);
+      });
+  }
+
+  // Function to perform movie search
+  function performSearch(movieData) {
     const searchInput = document.getElementById("searchInput");
     const searchResults = document.getElementById("searchResults");
     const searchTerm = searchInput.value.toLowerCase();
@@ -283,34 +42,105 @@ document.addEventListener('DOMContentLoaded', (event) => {
     if (searchTerm.trim() === "") {
       searchResults.style.display = "none";
     } else {
-      const filteredItems = itemList.filter((item) =>
-        item.toLowerCase().includes(searchTerm)
+      const filteredMovies = movieData.filter((movie) =>
+        movie.title.toLowerCase().includes(searchTerm)
       );
-      if (filteredItems.length === 0) {
+      if (filteredMovies.length === 0) {
         searchResults.innerHTML = "<p>No results found.</p>";
       } else {
-        displayItemList(filteredItems, searchResults);
+        displayItemList(filteredMovies, searchResults);
       }
       searchResults.style.display = "block";
     }
   }
-  function displayItemList(items, container) {
+
+  // Function to display filtered movie data
+  function displayItemList(items, container, maxItems = 12) {
     const ul = document.createElement("ul");
-    items.forEach((item) => {
-      const li = document.createElement("li");
-      const link = document.createElement("a");
-      link.textContent = item;
-      link.href = `../movies/${encodeURIComponent(item)}.html`;
-      li.appendChild(link);
-      ul.appendChild(li);
-    });
+    let visibleItemCount = maxItems;
+
+    function renderItems(startIndex, endIndex) {
+      for (let i = startIndex; i < endIndex; i++) {
+        const item = items[i];
+        if (!item) break;
+
+        const li = document.createElement("li");
+        const link = document.createElement("a");
+        link.textContent = item.title;
+        // Use the imageSrc property to construct the image URL
+        link.href = `../movies/${encodeURIComponent(item.title)}.html`;
+        li.dataset.tooltip = item.imageSrc; // Use the imageSrc property for the tooltip
+        li.appendChild(link);
+        ul.appendChild(li);
+      }
+    }
+
+    renderItems(0, visibleItemCount);
+
+    // Check if there are more items to show
+    if (items.length > visibleItemCount) {
+      const seeMoreLink = document.createElement("a");
+      seeMoreLink.textContent = "See More";
+      seeMoreLink.href = "#"; // You can set the actual link here
+      seeMoreLink.addEventListener("click", (event) => {
+        event.preventDefault();
+        visibleItemCount += maxItems; // Increase the visible item count
+        ul.innerHTML = ""; // Clear the current list
+        renderItems(0, visibleItemCount); // Display more items
+        if (visibleItemCount < items.length) {
+          ul.appendChild(seeMoreLink); // Add "See More" link if there are still more items
+        }
+        addTooltipListeners(); // Add tooltip listeners again after updating the list
+      });
+      ul.appendChild(seeMoreLink);
+    }
+
+    container.innerHTML = ""; // Clear the current container
     container.appendChild(ul);
+    addTooltipListeners();
   }
+
+
+  // Function to add tooltip listeners
+  function addTooltipListeners() {
+    const listItems = document.querySelectorAll("li[data-tooltip]");
+    listItems.forEach((li) => {
+      const link = li.querySelector("a");
+      const tooltipUrl = li.dataset.tooltip;
+      const tooltip = document.createElement("div");
+      tooltip.className = "tooltip";
+      tooltip.textContent = "Loading...";
+      li.addEventListener("mouseenter", () => {
+        tooltip.textContent = "Loading...";
+        tooltip.style.display = "block";
+        const tooltipImage = document.createElement("img");
+        tooltipImage.src = tooltipUrl;
+        tooltipImage.onload = () => {
+          tooltip.textContent = "";
+          tooltip.appendChild(tooltipImage);
+
+          // Set the maximum width and height of the image here
+          tooltipImage.style.maxWidth = "20%";
+          tooltipImage.style.maxHeight = "20%";
+        };
+      });
+      li.addEventListener("mouseleave", () => {
+        tooltip.style.display = "none";
+      });
+      li.appendChild(tooltip);
+    });
+  }
+
+
+  // Load movie data when the page loads
+  loadMovieData();
+
   const searchInput = document.getElementById("searchInput");
-  searchInput.addEventListener("input", performSearch);
+  searchInput.addEventListener("input", () => performSearch(movieData));
   const searchResults = document.getElementById("searchResults");
   searchResults.style.display = "none";
 });
+
 
 
 // Function to set a cookie
